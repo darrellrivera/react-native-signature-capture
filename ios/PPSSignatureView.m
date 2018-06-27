ac84a709b68a80ef0f3d5a527d73af156be4bb34
Darrell Rivera <darrell.rivera@decisiv.com>
2018-06-27 16:33:49 -0400

    Removes longpress functionality

--- ios/PPSSignatureView.m
+++ ios/PPSSignatureView.m
@@ -145,11 +145,6 @@ static PPSSignaturePoint ViewPointToGL(CGPoint viewPoint, CGRect bounds, GLKVect
 		UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
 		tap.cancelsTouchesInView = YES;
 		[self addGestureRecognizer:tap];
-
-		// Erase with long press
-		UILongPressGestureRecognizer *longer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPress:)];
-		longer.cancelsTouchesInView = YES;
-		[self addGestureRecognizer:longer];
 	}
 	else
 		[NSException raise:@"NSOpenGLES2ContextException" format:@"Failed to create OpenGL ES2 context"];
@@ -372,11 +367,6 @@ static PPSSignaturePoint ViewPointToGL(CGPoint viewPoint, CGRect bounds, GLKVect
 	}
 
 	[self setNeedsDisplay];
-}
-
-
-- (void)longPress:(UILongPressGestureRecognizer *)lp {
-	[self erase];
 }
 
 - (void)pan:(UIPanGestureRecognizer *)p {
